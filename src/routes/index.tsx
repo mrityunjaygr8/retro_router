471b1bacd5fd7d0eb3b397fa2acf40e146cab2b4
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-08-13 19:23:13 +0530

    initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/retroui/Button";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({
+  component: App,
+});
+
+function App() {
+  return <Button>Woo</Button>;
+}
