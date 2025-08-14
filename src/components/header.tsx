29ec99d07e992774cb5aeb4ffea775721158f1fe
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-08-14 22:05:05 +0530

    user's name added to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,9 @@ import { Link } from "@tanstack/react-router";
 import { Button } from "./retroui/Button";
 import { Text } from "./retroui/Text";
 import { useState } from "react";
+import { ListIcon } from "@phosphor-icons/react";
+import { useAtomValue } from "jotai";
+import userAtom from "@/lib/stores/user";
 
 const Links = () => {
   return (
@@ -28,24 +31,28 @@ const Header = () => {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+  const user = useAtomValue(userAtom);
   return (
-    <nav className="bg-white shadow-lg sticky top-0 z-50 mb-4">
+    <nav className=" bg-white border-solid border-b-8 sticky top-0 z-50 mb-4">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
           <div className="flex-shrink-0">
-            <Text as="h1">PARHAM</Text>
+            <Text as="h3">PARHAM</Text>
           </div>
 
           {/* Desktop Menu */}
           <div className="hidden md:flex items-center space-x-8">
             <Links />
+            <Text as="h6">
+              {user["first_name"]} {user["last_name"]}
+            </Text>
           </div>
 
           {/* Mobile Menu Button */}
           <div className="md:hidden flex items-center">
-            <Button variant="outline" onClick={toggleMenu}>
-              Menu
+            <Button variant="outline" size="icon" onClick={toggleMenu}>
+              <ListIcon />
             </Button>
           </div>
         </div>
