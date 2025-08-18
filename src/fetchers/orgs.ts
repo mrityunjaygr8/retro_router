73b59fd0638eb0a0d818711cb4a0ab42ef8b4677
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2025-08-18 17:41:05 +0530

    queryOptions properly typed

--- src/fetchers/orgs.ts
+++ src/fetchers/orgs.ts
@@ -1,11 +1,36 @@
 import { queryOptions, type UseQueryOptions } from "@tanstack/react-query";
 import api from "@/lib/api";
+import type { PaginatedData } from "@/lib/types";
 
-const useGetOrgs = (options?: Omit<UseQueryOptions, "queryKey" | "queryFn">) =>
+interface ListOrgs {
+  id: number;
+  name: string;
+  license: {
+    org: number;
+    max_assets: number;
+    max_users: number;
+    end_date: string;
+    features: Array<string>;
+  };
+  asset_count: number;
+  users: {
+    user_count: number;
+  };
+}
+
+const useGetOrgs = <TData = PaginatedData<ListOrgs>, TError = Error>(
+  options?: Omit<
+    UseQueryOptions<PaginatedData<ListOrgs>, TError, TData>,
+    "queryKey" | "queryFn"
+  >,
+) =>
   queryOptions({
     ...options,
     queryKey: ["orgs"],
-    queryFn: () => api.get("/api/v1/org/").then((res) => res.data),
+    queryFn: async (): Promise<PaginatedData<ListOrgs>> => {
+      const { data } = await api.get("/api/v1/org/");
+      return data;
+    },
   });
 
 export { useGetOrgs };
